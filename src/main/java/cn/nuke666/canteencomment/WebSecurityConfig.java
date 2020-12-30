57cdfd97a1c1304e40d687183feaa13fcbf265ce
ChinaNuke <chinanuke@nuke666.cn>
2020-12-30 14:42:39 +0800

    实现注册和登录功能

--- src/main/java/cn/nuke666/canteencomment/WebSecurityConfig.java
+++ src/main/java/cn/nuke666/canteencomment/WebSecurityConfig.java
@@ -28,8 +28,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-//                .antMatchers("/", "/register", "/js/**", "/css/**", "/fonts/**", "/img/**").permitAll()
-                .anyRequest().permitAll()
+                .antMatchers("/", "/register", "/js/**", "/css/**", "/fonts/**", "/img/**", "/login").permitAll()
+                .anyRequest().authenticated()
                 .and()
             .formLogin()
                 .loginPage("/login")
